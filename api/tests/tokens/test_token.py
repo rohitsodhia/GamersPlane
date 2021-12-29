5e64e2c6cc5e62317ef1a6679afd053a678b8ff0
Rohit Sodhia <sodhia.rohit@gmail.com>
2021-12-28 21:59:13 -0500

    Fix token test

--- api/tests/tokens/test_token.py
+++ api/tests/tokens/test_token.py
@@ -1,5 +1,12 @@
-from tokens.models import Token
+from factories.user_factory import UserFactory
+from tokens.models import Token, AccountActivationToken
+from users.models import User
 
 
 class TestToken:
-    pass
+    def test_save(self):
+        user = UserFactory()
+        user.save()
+        token = AccountActivationToken(user=user)
+        token.save()
+        assert token.token_type == Token.TokenTypes.ACCOUNT_ACTIVATION.value
