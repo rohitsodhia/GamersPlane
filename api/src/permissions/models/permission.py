807b719be1759d55803a863e460c13974e220580
Rohit Sodhia <sodhia.rohit@gmail.com>
2022-07-10 22:33:57 -0400

    Simplify the permissions model logic

--- api/src/permissions/models/permission.py
+++ api/src/permissions/models/permission.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class ValidPermissions(Enum):
-    ROLE_ADMIN = "role_admin_", "<role_id>"
+    ROLE_ADMIN = "role_admin_{role_id}"
 
 
 class Permission(models.Model):
@@ -17,25 +17,12 @@ class Permission(models.Model):
         self._valid_permission = False
         super().__init__(*args, **kwargs)
 
-    def set_permission(self, permission: ValidPermissions, *args):
+    def set_permission(self, permission: ValidPermissions, **kwargs):
         if type(permission) is not ValidPermissions:
             raise ValueError(f"'{permission}' is not a ValidPermission")
-        if type(permission.value) == tuple:
-            self._valid_permission = True
-            permission_val = ""
-            arg_key = 0
-            for v in permission.value:
-                if v[0] == "<":
-                    try:
-                        permission_val += str(args[arg_key])
-                    except IndexError:
-                        raise ValueError(
-                            f"Not enough arguments passed for '{permission.value}'"
-                        )
-                    arg_key += 1
-                else:
-                    permission_val += v
-            self.permission = permission_val
+
+        if "{" in permission.value:
+            self.permission = permission.value.format(**kwargs)
         else:
             self.permission = permission.value
 
