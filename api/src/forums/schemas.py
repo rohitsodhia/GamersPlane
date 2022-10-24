7150a586b977b9c151d8edd1a93f3bcf6ee2a858
Rohit Sodhia <sodhia.rohit@gmail.com>
2022-10-23 23:05:59 -0400

    Create schema for forum permissions

--- api/src/forums/schemas.py
+++ api/src/forums/schemas.py
@@ -1,9 +1,16 @@
 from lib2to3.pgen2.token import OP
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, create_model
 
 from forums.models.forum import Forum
+from permissions.models.permission import ForumPermissions
+
+
+PermissionsDict = create_model(
+    "PermissionsDict",
+    **{permission.value: (bool, False) for permission in ForumPermissions}
+)
 
 
 class CreateForumInput(BaseModel):
