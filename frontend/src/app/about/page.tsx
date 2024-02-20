a2ba2f710e97114dd58ec0b930f98509ff4093e4
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-02-19 22:39:24 -0500

    Create about us page

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,63 @@
+import Headerbar from "@/components/Headerbar";
+
+export default function About() {
+    return (
+        <div className="max-w-screen-xl mx-auto">
+            <Headerbar>About Gamers&apos; Plane</Headerbar>
+            <div className="hb-container">
+                <p>
+                    I played my first RPG back in high school (2000 or 2001, not
+                    sure). I don&apos;t remember exactly how it happened, but
+                    basically, a friend was already setting up charater
+                    generation for a D&D 3e game during lunch, and somehow I was
+                    invited in. I remember creating some sort of ranger and had
+                    a blast. That first game led to many more over the next few
+                    years, starting with D&D 3e and going to D&D 3.5, Cthulhu,
+                    and a small variety of games. I looked forward to the games,
+                    to the characters, to the story.
+                </p>
+
+                <p>
+                    When I got to college, to game more, I decided to join a
+                    game though the RPG club on campus. Someone was running a
+                    Legend of the Five Rings game, and doing character
+                    generation at the campus center. As I found the GM, he fell
+                    right within the geek stereotype: heavy-set, awash with body
+                    odor, generally unhygenic; I was surprised to find the
+                    stereotype true. Though I had made it clear I didn&apos;t
+                    know the game, the GM was annoyed at my lack of knowledge. I
+                    also didn&apos;t own dice at the time, and though the GM had
+                    a bucket of dice (enough that he could have been rolling one
+                    a minute and not been through them by the time the game
+                    ended), he was annoyed at having to lend me a set. The whole
+                    experience soured me, and 20 minutes in, I excused myself to
+                    the bathroom and left.
+                </p>
+
+                <p>
+                    I decided to see what options I had for online gaming. A
+                    friend recommended trying play-by-post gaming through a
+                    website he was on. I found the idea neat, but quickly got
+                    annoyed at the lack fo community on the site, and how bland
+                    it felt. I wasn&apos;t impressed and decided to do something
+                    about it.
+                </p>
+
+                <p>
+                    After many iterations and years and failures, I finally got
+                    Gamers&apos; Plane off the ground at the end of 2013. Since
+                    then we&apos;ve been going strong, gaining new players every
+                    day, and new games all the time. I&apos;m excited to bring
+                    players together from around the world, and hope to keep
+                    making great tools to let people get into RPG gaming.
+                </p>
+
+                <p className="text-lg font-bold text-right mb-0">Rohit</p>
+
+                <p className="text-right mt-1">
+                    Gamers&apos; Plane&apos;s Developer
+                </p>
+            </div>
+        </div>
+    );
+}
