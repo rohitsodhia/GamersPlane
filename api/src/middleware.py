8171feb2a69f95bfd2a730cb5b71444e536e9afb
Rohit Sodhia <sodhia.rohit@gmail.com>
2024-10-15 18:31:03 -0400

    Change JWT algo field to list

--- api/src/middleware.py
+++ api/src/middleware.py
@@ -1,11 +1,9 @@
 import jwt
-
-from fastapi import Request
 from asgiref.sync import sync_to_async
+from fastapi import Request
 
 import envs
 from globals import g
-
 from users.models import User
 
 
@@ -19,14 +17,14 @@ async def validate_jwt(request: Request, call_next):
             jwt_body = jwt.decode(
                 token,
                 envs.JWT_SECRET_KEY,
-                algorithms=envs.JWT_ALGORITHM,
+                algorithms=[envs.JWT_ALGORITHM],
             )
         except jwt.ExpiredSignatureError:
             return await call_next(request)
 
     try:
         g.current_user = await sync_to_async(User.objects.get)(id=jwt_body["user_id"])
-    except User.DoesNotExist:
+    except:
         pass
 
     response = await call_next(request)
