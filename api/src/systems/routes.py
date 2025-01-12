24e69463ac166a68f6e9bb214aa4e9180a48ece7
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-01-11 20:11:12 -0500

    Update systems routes to SQLA

--- api/src/systems/routes.py
+++ api/src/systems/routes.py
@@ -1,16 +1,29 @@
 from fastapi import APIRouter, Body
 
+from database import DBSessionDependency
+from models import System
 from systems import schemas
-from systems.models import System
 
 systems = APIRouter(prefix="/systems")
 
 
 @systems.get("/", response_model=schemas.GetSystemsResponse)
-def get_systems(basic: bool = Body(False, embed=True)):
-    systems = System.objects
-    if basic:
-        systems = systems.basic()
-    systems = systems.order_by("sortName").values()
+def get_systems(basic: bool = Body(False, embed=True), db_session=DBSessionDependency):
+    systems = System.get_all()
+    systems_return = []
+    for system in systems:
+        if basic:
+            systems_return.append(
+                {
+                    "id": system.id,
+                    "name": system.name,
+                    "genres": [genre.genre for genre in system.genres],
+                    "has_char_sheet": system.has_char_sheet,
+                }
+            )
+        else:
+            system_dict = system.__dict__
+            system_dict.pop("sort_name")
+            systems_return.append(system_dict)
 
-    return {"systems": [system for system in systems]}
+    return {"systems": systems_return}
