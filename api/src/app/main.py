e417cecd26a405326d64581b9ce55750e572e827
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-01-19 23:43:14 -0500

    Rename app.py to main.py

--- api/src/app/main.py
+++ api/src/app/main.py
@@ -1,17 +1,18 @@
 from random import seed
 
-import middleware
-from auth.routes import auth
-from database import get_db_session
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
+
+from app import middleware
+from app.auth.routes import auth
+from app.database import get_db_session
 
 # from forums.forums_routes import forums
 # from permissions.roles_routes import roles
 # from referral_links.routes import referral_links
 # from systems.routes import systems
-from users.routes import users
+from app.users.routes import users
 
 # from permissions.permissions_routes import permissions
 
