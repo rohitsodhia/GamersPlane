c500deafab7216099aafc3af64206e94551fd3ef
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-02-06 12:45:36 -0500

    Dev/next js setup (#77)
    * Change frontend from Angular to NextJS
    
    * Create frontend dockerfile and compose configs
    
    * Configure nginx for frontend
    
    * Set up compose file to use env to build nextjs project
    
    * Don't use dockerfile for nginx, add to nginx conf to allow websockets to FE in dev

--- frontend/src/app/shared/api-response.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface APIResponse {
-	data?: {} | any[];
-	errors?: {};
-}
--- frontend/src/app/shared/referral-links-get-api-request.interface.ts
+++
@@ -1,8 +0,0 @@
-import { APIResponse } from './api-response.interface';
-import { ReferralLink } from './referral-link.interface';
-
-export interface ReferralLinksGetApiResponse extends APIResponse {
-	data?: {
-		referralLinks: ReferralLink[]
-	}
-}
