0f66f543603be7b8462bb6bfb58350b124b80705
Rohit Sodhia <sodhia.rohit@gmail.com>
2025-05-30 18:12:57 -0400

    Update database setup and initialization

--- api/src/app/main.py
+++ api/src/app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from random import seed
 
 from fastapi import Depends, FastAPI
@@ -5,7 +6,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app import middleware
 from app.auth.routes import auth
-from app.database import get_db_session
+from app.configs import configs
+from app.database import get_db_session, session_manager
 
 # from forums.forums_routes import forums
 # from permissions.roles_routes import roles
@@ -18,13 +20,28 @@ from app.users.routes import users
 seed()
 
 
-def create_app():
+def create_app(init_db=True) -> FastAPI:
+    if init_db:
+        session_manager.init(
+            host=configs.DATABASE_HOST,
+            user=configs.DATABASE_USER,
+            password=configs.DATABASE_PASSWORD,
+            database=configs.DATABASE_DATABASE,
+        )
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        yield
+        if init_db and session_manager._engine is not None:
+            await session_manager.close()
+
     app = FastAPI(
         dependencies=[
             Depends(middleware.validate_jwt),
             Depends(get_db_session),
             Depends(middleware.check_authorization),
-        ]
+        ],
+        lifespan=lifespan,
     )
 
     app.add_middleware(
