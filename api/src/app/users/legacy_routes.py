f78b9125155516c4be9c09e5b660b3721f45d64a
Rohit Sodhia <sodhia.rohit@gmail.com>
2026-02-04 21:38:12 -0500

    Create character filters for header

--- api/src/app/users/legacy_routes.py
+++ api/src/app/users/legacy_routes.py
@@ -13,4 +13,9 @@ async def get_header(db_session: DBSessionDependency, authed_user: AuthedUser):
     character_repository = CharacterRepository(db_session, authed_user)
 
     characters = await character_repository.get_header_characters()
-    ic(characters)
+    if len(characters) > 0 and characters[0]["isFavorite"]:
+        characters = filter(lambda x: x["isFavorite"], characters)
+    elif len(characters) > 0:
+        characters = characters[:6]
+
+    return {"characters": characters}
--- api/src/app/users/legacy_schemas.py
+++ api/src/app/users/legacy_schemas.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 
 
+class HeaderCharacters(BaseModel):
+    id: int
+    isFavorite: bool
+    label: str
+    system: str
+
+
 class GetHeaderResponse(BaseModel):
-    pass
+    characters: list[HeaderCharacters]
