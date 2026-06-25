bae0ba104f2df9587fda1998e597c2ea5418aaf6
Rohit Sodhia <sodhia.rohit@gmail.com>
2026-06-25 10:37:28 -0400

    Dev/loading (#100)
    * Set up loading spinner
    
    * Update text length based on updated biome settings

--- frontend/src/routes/register/success.tsx
+++ frontend/src/routes/register/success.tsx
@@ -10,18 +10,14 @@ function RouteComponent() {
 			<h1 className="headerbar">Thank you for registering!</h1>
 			<div className="hb-margined">
 				<p>
-					An email has been sent to you with instructions on how to activate
-					your account.
+					An email has been sent to you with instructions on how to activate your
+					account.
 				</p>
 				<p>
-					Please make sure <strong>contact@gamersplane.com</strong> is
-					whitelisted on your email account. If you do not recieve an email in
-					your inbox, please check your spam folder. If you still haven't gotten
-					a mail, you can try{" "}
-					<a href="/register/resendActivation/">
-						resending an activation email
-					</a>
-					.
+					Please make sure <strong>contact@gamersplane.com</strong> is whitelisted on
+					your email account. If you do not recieve an email in your inbox, please check
+					your spam folder. If you still haven't gotten a mail, you can try{" "}
+					<a href="/register/resendActivation/">resending an activation email</a>.
 				</p>
 			</div>
 		</div>
