8bf52f67cd0e768aaf243fc41b15c47c47f3d0ed
Rohit Sodhia <sodhia.rohit@gmail.com>
2026-06-26 11:53:21 -0400

    Set up resend activation pgae

--- frontend/src/queries/register.ts
+++ frontend/src/queries/register.ts
@@ -15,3 +15,15 @@ export const register = async (registrationData: {
 	}
 	return res.json();
 };
+
+export const resendActivation = async (data: { email: string }) => {
+	const res = await apiFetch("/auth/resendActivation", {
+		method: "POST",
+		body: JSON.stringify(data),
+	});
+	if (!res.ok) {
+		const { errors } = await res.json();
+		throw new ApiError(res.status, errors);
+	}
+	return res.json();
+};
