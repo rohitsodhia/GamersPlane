074417950172f14deecbf4f089ef6e5ae64b7187
Rohit Sodhia <sodhia.rohit@gmail.com>
2026-07-06 08:27:05 -0400

    Move custom to beginning of system list

--- api/src/app/systems/routes.py
+++ api/src/app/systems/routes.py
@@ -13,33 +13,37 @@ systems = APIRouter(prefix="/systems")
 async def get_systems(db_session: DBSessionDependency, basic: bool = False):
     system_repository = SystemRepository(db_session)
     systems_return = []
+    custom_dict = {}
     for system in await system_repository.get():
+        system_dict = {}
         if basic:
-            systems_return.append(
-                {
-                    "id": system.id,
-                    "name": system.name,
-                    "genres": [genre.genre for genre in system.genres],
-                    "has_char_sheet": system.has_char_sheet,
-                }
-            )
+            system_dict = {
+                "id": system.id,
+                "name": system.name,
+                "genres": [genre.genre for genre in system.genres],
+                "has_char_sheet": system.has_char_sheet,
+            }
         else:
-            systems_return.append(
-                {
-                    "id": system.id,
-                    "name": system.name,
-                    "sort_name": system.sort_name,
-                    "publisher": {
-                        "name": system.publisher.name,
-                        "website": system.publisher.website,
-                    }
-                    if system.publisher
-                    else None,
-                    "genres": [genre.genre for genre in system.genres],
-                    "basics": system.basics,
-                    "has_char_sheet": system.has_char_sheet,
-                    "enabled": system.enabled,
+            system_dict = {
+                "id": system.id,
+                "name": system.name,
+                "sort_name": system.sort_name,
+                "publisher": {
+                    "name": system.publisher.name,
+                    "website": system.publisher.website,
                 }
-            )
+                if system.publisher
+                else None,
+                "genres": [genre.genre for genre in system.genres],
+                "basics": system.basics,
+                "has_char_sheet": system.has_char_sheet,
+                "enabled": system.enabled,
+            }
+
+        if system.id == "custom":
+            custom_dict = system_dict
+            continue
+        systems_return.append(system_dict)
+    systems_return = [custom_dict] + systems_return
 
     return {"systems": systems_return}
