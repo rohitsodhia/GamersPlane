7c185bd6fabac90aab40202b6155936c7c701744
Rohit Sodhia <sodhia.rohit@gmail.com>
2026-08-20 21:45:20 -0400

    Wrong type on system id

--- api/src/app/models/game_allowed_systems.py
+++ api/src/app/models/game_allowed_systems.py
@@ -8,4 +8,4 @@ class GameAllowedSystem(Base):
     __tablename__ = "game_allowed_systems"
 
     game_id: Mapped[int] = mapped_column(ForeignKey("games.id"), primary_key=True)
-    system_id: Mapped[int] = mapped_column(ForeignKey("systems.id"), primary_key=True)
+    system_id: Mapped[str] = mapped_column(ForeignKey("systems.id"), primary_key=True)
