cd9f73606a853ddef953e93a12dac703c16c9a3a
Rohit Sodhia <sodhia.rohit@gmail.com>
2026-08-31 14:23:12 -0400

    Removing old permissions route group

--- api/src/app/permissions/functions.py
+++
@@ -1,9 +0,0 @@
-from app.permissions.models import Permission
-from app.permissions.models.permission import ValidPermissions
-
-
-def create_permission(permission: ValidPermissions, **kwargs) -> Permission:
-    permission_obj = Permission()
-    permission_obj.set_permission(permission, **kwargs)
-    permission_obj.save()
-    return permission_obj
